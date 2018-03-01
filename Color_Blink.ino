99ff88496e95ec7a568feed3d1c24015b55f0e47
CSA-ANS <33459799+CSA-ANS@users.noreply.github.com>
2018-03-01 09:06:25 -0600

    Add files via upload

---
+++ Color_Blink.ino
@@ -0,0 +1,32 @@
+const int red=9;
+const int green=10;
+const int blue=11;
+
+int time=1000;
+int multiple=2;
+
+void setup() {
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(red, HIGH);
+  delay(time);
+  digitalWrite(green, HIGH);
+  delay(time);
+  digitalWrite(red, LOW);
+  delay(time);
+  digitalWrite(blue, HIGH);
+  delay(time);
+  digitalWrite(green, LOW);
+  delay(time);
+  digitalWrite(red, HIGH);
+  delay(time);
+  digitalWrite(green, HIGH);
+  delay(time*=multiple);
+  digitalWrite(blue, LOW);
+  digitalWrite(green, LOW);
+  time/=multiple;
+}
---
+++ Expansion3.ino
@@ -0,0 +1,17 @@
+const int red1=2;
+const int green1=3;
+void setup() {
+  pinMode(red1, OUTPUT);
+  pinMode(green1, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(red1, HIGH);
+  delay(1000);
+  digitalWrite(green1, HIGH);
+  delay(1000);
+  digitalWrite(green1, LOW);
+  delay(1000);
+  digitalWrite(red1, LOW);
+  delay(1000);
+}
