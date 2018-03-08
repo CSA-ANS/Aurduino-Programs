d9167f27e25c9814773545be719e820b994b2eff
CSA-ANS <33459799+CSA-ANS@users.noreply.github.com>
2018-03-08 09:01:58 -0600

    Add files via upload

---
+++ Expansion4.ino
@@ -0,0 +1,53 @@
+const int red1=2;
+const int green1=3;
+const int blue1=4;
+const int yellow1=5;
+const int yellow2=6;
+const int blue2=7;
+const int green2=8;
+const int red2=9;
+void setup() {
+  pinMode(red1, OUTPUT);
+  pinMode(green1, OUTPUT);
+  pinMode(blue1, OUTPUT);
+  pinMode(yellow1, OUTPUT);
+  pinMode(red2, OUTPUT);
+  pinMode(green2, OUTPUT);
+  pinMode(blue2, OUTPUT);
+  pinMode(yellow2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(red1, HIGH);
+  digitalWrite(red2, HIGH);
+  delay(100);
+  digitalWrite(green1, HIGH);
+  digitalWrite(green2, HIGH);
+  delay(100);
+  digitalWrite(blue1, HIGH);
+  digitalWrite(blue2, HIGH);
+  delay(100);
+  digitalWrite(yellow1, HIGH);
+  digitalWrite(yellow2, HIGH);
+  delay(100);
+  digitalWrite(yellow1, LOW);
+  digitalWrite(yellow2, LOW);
+  delay(100);
+  digitalWrite(blue1, LOW);
+  digitalWrite(blue2, LOW);
+  delay(100);
+  digitalWrite(green1, LOW);
+  digitalWrite(green2, LOW);
+  delay(100);
+  digitalWrite(red1, LOW);
+  digitalWrite(red2, LOW);
+  delay(250);
+}
+
+
+
+
+
+
+
+
---
+++ Tilt_Blink_Expansion.ino
@@ -0,0 +1,61 @@
+const int rgb[] = {9,10,11};
+//makes array with all pin numbers
+const int time=200;
+//time is constant 
+const int switchPin=2;
+//defines the tilt switch pin number
+void setup() {
+  for (int i=0; i<3; i++) pinMode(rgb[i], OUTPUT);
+  //makes all the pins in the array outputs
+  pinMode(switchPin, INPUT);
+  //makes the switch an input
+}
+void dot(){
+  //dot function
+  digitalWrite(9, HIGH);
+  delay(time);
+  digitalWrite(9, LOW);
+  delay(time);
+}
+void dash(){
+  //dash function
+  digitalWrite(9, HIGH);
+  delay(time*3);
+  digitalWrite(9, LOW);
+  delay(time);
+}
+void dot_dash(){
+  //combines them
+  dot();
+  dash();
+}
+void dash_dot(){
+  //combines them
+  dash();
+  dot();
+}
+
+void loop() {
+  int bounce1=digitalRead(switchPin);
+  delay(25);
+  int bounce2=digitalRead(switchPin);
+//debounces the switch to make sure it is turned off
+  while ((bounce1==bounce2)&&(bounce1==LOW)){//while it is closed
+    for (int i=0; i<3; i++) digitalWrite(rgb[i], LOW);//turns all LEDs off
+    for (int i=0; i<3; i++){//does the dot twice
+      dot();
+    }
+    for (int i=0; i<3; i++){//does the dash twice
+      dash();
+    }
+    for (int i=0; i<3; i++){//does the dot twice again
+      dot();
+    }
+    bounce1=digitalRead(switchPin);
+    delay(25);
+    bounce2=digitalRead(switchPin);//debpunces the switch again
+  }
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, HIGH);//if it gets out of the while loop it makes it blue
+}
