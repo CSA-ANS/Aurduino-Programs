42879514cd3e39a2151eeae992d03e2b4aaca8a4
CSA-ANS <33459799+CSA-ANS@users.noreply.github.com>
2018-03-26 19:52:06 -0500

    Update Expansion4.ino
    Added comments at beginning and throughout the program
    used functions when possible
    put open brackets at same position as closing brackets

--- Expansion4.ino
+++ Expansion4.ino
@@ -1,3 +1,5 @@
+// this program turns on 8 LEDs in order and then turns them off backwards
+//lines define all the pins
 const int red1=2;
 const int green1=3;
 const int blue1=4;
@@ -6,7 +8,23 @@ const int yellow2=6;
 const int blue2=7;
 const int green2=8;
 const int red2=9;
-void setup() {
+//function created to turn two pins on 
+void pinson(int pin1, int pin2)
+{
+  digitalWrite(pin1, HIGH);
+  digitalWrite(pin2, HIGH);
+  delay(100);
+}
+// turns two pins off
+void pinsoff(int pin1, int pin2)
+{
+  digitalWrite(pin1, LOW);
+  digitalWrite(pin2, LOW);
+  delay(100);
+}
+//make all the pins the correct type
+void setup() 
+{
   pinMode(red1, OUTPUT);
   pinMode(green1, OUTPUT);
   pinMode(blue1, OUTPUT);
@@ -16,32 +34,19 @@ void setup() {
   pinMode(blue2, OUTPUT);
   pinMode(yellow2, OUTPUT);
 }
-
-void loop() {
-  digitalWrite(red1, HIGH);
-  digitalWrite(red2, HIGH);
-  delay(100);
-  digitalWrite(green1, HIGH);
-  digitalWrite(green2, HIGH);
-  delay(100);
-  digitalWrite(blue1, HIGH);
-  digitalWrite(blue2, HIGH);
-  delay(100);
-  digitalWrite(yellow1, HIGH);
-  digitalWrite(yellow2, HIGH);
-  delay(100);
-  digitalWrite(yellow1, LOW);
-  digitalWrite(yellow2, LOW);
-  delay(100);
-  digitalWrite(blue1, LOW);
-  digitalWrite(blue2, LOW);
-  delay(100);
-  digitalWrite(green1, LOW);
-  digitalWrite(green2, LOW);
-  delay(100);
-  digitalWrite(red1, LOW);
-  digitalWrite(red2, LOW);
-  delay(250);
+//runs this again and again
+void loop() 
+{
+  pinson(red1, red2);
+  pinson(green1, green2);
+  pinson(blue1, blue2);
+  pinson(yellow1, yellow2)
+  //turns on all the pins
+  pinsoff(yellow1, yellow2);
+  pinsoff(blue1, blue2);
+  pinsoff(green1, green2);
+  pinsoff(red1, red2);
+  //turns off all the pins going the opposite direction 
 }
 
 
