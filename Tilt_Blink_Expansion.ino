546727174dd25d5f0fc1b30a9a7253b418cff078
CSA-ANS <33459799+CSA-ANS@users.noreply.github.com>
2018-03-26 20:00:10 -0500

    Update Tilt_Blink_Expansion.ino
    Added comments to start and throughout code
    moved comments to correct position
    fixed few grammatical errors in comments
    put open brackets in the same position as closed brackets

--- Tilt_Blink_Expansion.ino
+++ Tilt_Blink_Expansion.ino
@@ -1,59 +1,66 @@
-const int rgb[] = {9,10,11};
-//makes array with all pin numbers
+//This program is continously blue and gives an SOS when it is shaken or moved
+const int rgb[] = {9,10,11};//makes array with all pin numbers
 const int time=200;
-//time is constant 
-const int switchPin=2;
-//defines the tilt switch pin number
-void setup() {
+const int switchPin=2;//defines the tilt switch pin number
+//defines pin types
+void setup() 
+{
   for (int i=0; i<3; i++) pinMode(rgb[i], OUTPUT);
-  //makes all the pins in the array outputs
   pinMode(switchPin, INPUT);
-  //makes the switch an input
 }
-void dot(){
+void dot()
+{
   //dot function
   digitalWrite(9, HIGH);
   delay(time);
   digitalWrite(9, LOW);
   delay(time);
 }
-void dash(){
+void dash()
+{
   //dash function
   digitalWrite(9, HIGH);
   delay(time*3);
   digitalWrite(9, LOW);
   delay(time);
 }
-void dot_dash(){
+void dot_dash()
+{
   //combines them
   dot();
   dash();
 }
-void dash_dot(){
+void dash_dot()
+{
   //combines them
   dash();
   dot();
 }
 
-void loop() {
+void loop() 
+{
+  //debounces the switch to make sure it is turned off
   int bounce1=digitalRead(switchPin);
   delay(25);
   int bounce2=digitalRead(switchPin);
-//debounces the switch to make sure it is turned off
-  while ((bounce1==bounce2)&&(bounce1==LOW)){//while it is closed
+  while ((bounce1==bounce2)&&(bounce1==LOW))//while it is closed
+  {
     for (int i=0; i<3; i++) digitalWrite(rgb[i], LOW);//turns all LEDs off
-    for (int i=0; i<3; i++){//does the dot twice
+    for (int i=0; i<3; i++)//does the dot thrice
+    {
       dot();
     }
-    for (int i=0; i<3; i++){//does the dash twice
+    for (int i=0; i<3; i++)//does the dash thrice
+    {
       dash();
     }
-    for (int i=0; i<3; i++){//does the dot twice again
+    for (int i=0; i<3; i++)//does dot thrice again
+    {
       dot();
     }
     bounce1=digitalRead(switchPin);
     delay(25);
-    bounce2=digitalRead(switchPin);//debpunces the switch again
+    bounce2=digitalRead(switchPin);//debounces the switch again
   }
   digitalWrite(9, LOW);
   digitalWrite(10, LOW);
